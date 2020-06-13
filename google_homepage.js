66c575788f5cbd6759e0e1a4f46e4f6b80f8b1a8
Megan Cornell <53664619+mcornell-pixel@users.noreply.github.com>
2020-06-13 09:33:35 -0400

    update two files

---
+++ google_homepage.js
@@ -0,0 +1,43 @@
+/* var text =
+document.getElementById("google_logo");
+var str = text.innerHTML,
+    reg = /blue|red|yellow|blue|green|red/ig;
+    
+    var toStr = String(reg);
+    var color = (toStr.replace('\/g', '|')).substring(1);
+    
+    var colors = color.split("|");
+
+    if (colors.indexOf("blue") > -1) {
+        str = str.replace(/G/g, 
+            '<span style="color:blue;">G</span>');
+    }
+
+    if (colors.indexOf("red") > -1) {
+        str = str.replace(/o/g,
+            '<span style="color:red;">o</span>');
+    }
+
+    if (colors.indexOf("yellow") > -1) {
+        str = str.replace(/o/g,
+            '<span style="color:yellow;">o</span>');
+
+    }
+
+    if (colors.indexOf("blue") > -1) {
+        str = str.replace(/g/g,
+            '<span style="color:blue;">g</span>');
+
+    }
+
+    if (colors.indexOf("green") > -1) {
+        str = str.replace(/l/g,
+            '<span style="color:yellow;">l</span>');
+
+    }
+    if (colors.indexOf("red") > -1) {
+        str = str.replace(/e/g,
+            '<span style="color:yellow;">e</span>');
+    }
+
+    document.getElementById("updated").innerHTML = str; */
